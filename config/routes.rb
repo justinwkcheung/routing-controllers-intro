5fd052ca996422fb9d0d10d510da2be5fad78e2a
Justin Cheung <justinwkcheung@github.com>
2017-01-30 13:16:39 -0500

    Create contest route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/welcome', to: 'pages#welcome', as: 'welcome'
   get '/', to: 'pages#welcome'
   get '/about', to: 'pages#about', as: 'about'
+  get '/contests', to: 'pages#contest', as: 'contests'
 
 end
